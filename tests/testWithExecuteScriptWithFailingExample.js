1bd92aac51e7fca42ec2b9fa71bbc73d68cb0d60
marikomands <mariko.mands@gmail.com>
2025-07-10 14:50:33 +0100

    updated

---
+++ tests/testWithExecuteScriptWithFailingExample.js
@@ -0,0 +1,24 @@
+const { By, Builder, Browser, until } = require("selenium-webdriver");
+
+(async function firstTest() {
+  let driver;
+
+  try {
+    driver = await new Builder().forBrowser(Browser.CHROME).build();
+
+    await driver.navigate().to("http://www.tesco.com");
+    // Since this is exact match query it fails.  h=205&;w=205 is not matching
+    await driver.executeScript(`document.querySelector("img[src='https://digitalcontent.api.tesco.com/v2/media/homepage/5373f026-fdea-47a2-82ea-ce7660bb7a86/Pizzaexpress.jpeg?h=205&;w=205']")
+        ?.scrollIntoView();
+        `);
+    console.log("✅ Test passed: element was scrolled into view.");
+
+    await driver.sleep(5000);
+  } catch (e) {
+    console.log("test failed", e);
+  } finally {
+    await driver.quit();
+  }
+})();
+
+// This code navigates to the Amazon UK homepage and scrolls to the "Your Account" link.
