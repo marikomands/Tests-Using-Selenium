090e47e53125c06827af01e804e18975834d4670
marikomands <mariko.mands@gmail.com>
2025-09-24 15:50:16 +0100

    Created

--- tests/dragAndDropTest.js
+++ tests/dragAndDropTest.js
@@ -1,25 +1,32 @@
-const { By, Builder, Browser } = require("selenium-webdriver");
+const { By, Builder, until } = require("selenium-webdriver");
+const assert = require("assert");
 
 (async function dragAndDropTest() {
-  let driver = await new Builder().forBrowser(Browser.CHROME).build();
-
+  let driver = await new Builder().forBrowser("chrome").build();
   try {
-    await driver.get(
-      "https://www.selenium.dev/selenium/web/droppableItems.html"
-    );
+    await driver.get("https://jqueryui.com/droppable/");
+
+    await driver.sleep(2000); // ページが表示されるか確認
+
+    await driver.switchTo().frame(0); // iframe内にあるので切り替えが必要
+
+    let title = await driver.getTitle();
+    assert.equal("Droppable | jQuery UI", title);
+
+    await driver.wait(until.elementLocated(By.id("draggable")), 1000);
     const srcEle = await driver.findElement(By.id("draggable"));
     const targetEle = await driver.findElement(By.id("droppable"));
+
     await driver
       .actions({ async: true })
       .dragAndDrop(srcEle, targetEle)
       .perform();
 
     console.log("✅ Test passed: element dragged.");
-
     await driver.sleep(2000);
   } catch (e) {
-    console.log("test failed", e);
+    console.error("❌ Test failed:", e);
   } finally {
     await driver.quit();
   }
-});
+})();
